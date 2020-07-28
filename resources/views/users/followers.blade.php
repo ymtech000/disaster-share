1d1fd6b94ab51537d9004b503fd688e5fdea43c0
Yuuta Miyauchi <pog.tor.528@gmail.com>
2020-07-28 11:56:30 +0000

    ユーザー情報の共通化

--- resources/views/users/followers.blade.php
+++ resources/views/users/followers.blade.php
@@ -1,29 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="error">
-        @if (\Session::has('error'))
-            <div class="alert alert-error" id="error">
-                {!! \Session::get('error') !!}
-            </div>
-        @endif
-    </div>
-   <div class='form-row'>
-        <div class="offset-md-2 col-md-3">
-            <span style="text-align:center;">
-                @include('users.card', ['user'=>$user])
-            </span>
-        </div>
-        <div class="col-md-6">
-            <div class="name">
-                <p style="font-weight:bold;">{{$user->name}}</p>
-                <p>{{$user->introduction}}</p>
-            </div>
-        </div>
-    </div>
-    <div class="navtabs">
-        @include('users.navtabs', ['user' => $user])
-    </div>
+    @include('commons.user_info', ['user' => $user])
     @include('users.users', ['users' => $users])
 <style>
         .name{
