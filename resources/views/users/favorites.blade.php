1350407e19ca5f3b19f4ad7b01e09b48f6ae2fc6
Yuuta Miyauchi <pog.tor.528@gmail.com>
2020-08-03 05:02:42 +0000

    ファイルの読み込み

--- resources/views/users/favorites.blade.php
+++ resources/views/users/favorites.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    @include('commons.user_info', ['user' => $user])
+    @include('users.user_info', ['user' => $user])
     @if (count($alerts) > 0)
         @include('alerts.alerts', ['alerts' => $alerts])
     @endif
