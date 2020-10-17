1a5315498b24909a950729a2c0c8071bdbb79654
yuta.miyauchi <pog.tor.528@gmail.com>
2020-10-17 19:48:44 +0900

    場合分けの変更

--- public/js/users_response.js
+++ public/js/users_response.js
@@ -3,12 +3,12 @@ $(window).resize(function(){
     //windowの幅をxに代入
     var winsize = $(window).width();
     for (var i = 0; i<users.length; i++) {
-        if (991 < winsize){
-            $('#'+users[i].id).addClass('col-sm-3').removeClass('col-sm-4');
-        }else if(768< winsize <= 991){
-            $('#'+users[i].id).addClass('col-sm-4').removeClass('col-sm-3');
+        if (768 < winsize){
+            $('#'+users[i].id).addClass('col-sm-4').removeClass('col-sm-3 col-sm-6');
+        }else if(574< winsize && winsize <= 768){
+            $('#'+users[i].id).addClass('col-sm-6').removeClass('col-sm-4 col-sm-3');
         }else{
-            $('#'+users[i].id).addClass('col-sm-3').removeClass('col-sm-4');
+            $('#'+users[i].id).addClass('col-sm-3').removeClass('col-sm-6 col-sm-4');
         }
     }
 });
